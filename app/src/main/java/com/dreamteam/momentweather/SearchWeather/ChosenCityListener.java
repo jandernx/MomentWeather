226ec46c8d1299f11ebe02508b3622edc796dc27
jandernx <janderr97@gmail.com>
2017-12-13 19:40:29 +0200

    Update ChosenCityListener.java

--- app/src/main/java/com/dreamteam/momentweather/SearchWeather/ChosenCityListener.java
+++ app/src/main/java/com/dreamteam/momentweather/SearchWeather/ChosenCityListener.java
@@ -1,8 +1,5 @@
 package com.dreamteam.momentweather.SearchWeather;
 
-/**
- * Created by roman on 06.07.17.
- */
 
 public interface ChosenCityListener {
     void onCityChosen(String cityName, double latitude, double longitude);
